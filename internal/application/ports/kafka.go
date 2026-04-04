cb44cfe47c2db19379672724cdea7254c028a785
khanhminhtk <khanhminh.gt.154203@gmail.com>
2026-04-04 19:12:27 +0700

    feat(minio): add ports for minio)

---
+++ internal/application/ports/kafka.go
@@ -0,0 +1,33 @@
+package ports
+
+import "context"
+
+type KafkaMessage struct {
+	Key     []byte
+	Value   []byte
+	Headers map[string]string
+}
+
+type PublishMessageInput struct {
+	Topic   string
+	Message KafkaMessage
+}
+
+type ConsumeMessage struct {
+	Topic     string
+	Partition int
+	Offset    int64
+	Message   KafkaMessage
+}
+
+type MessageHandler func(ctx context.Context, msg ConsumeMessage) error
+
+type KafkaPublisher interface {
+	Publish(ctx context.Context, input PublishMessageInput) error
+	Close() error
+}
+
+type KafkaConsumer interface {
+	Consume(ctx context.Context, topic string, groupID string, handler MessageHandler) error
+	Close() error
+}
