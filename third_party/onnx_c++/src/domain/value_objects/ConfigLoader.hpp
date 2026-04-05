d39b870d0d3a7dd85169e73777761a2177db6ee4
khanhminhtk <khanhminh.gt.154203@gmail.com>
2026-04-05 16:15:20 +0700

    feat(domain onnx): add domain folder for onnx

--- third_party/onnx_c++/src/domain/value_objects/ConfigLoader.hpp
+++ third_party/onnx_c++/src/domain/value_objects/ConfigLoader.hpp
@@ -1,35 +1,34 @@
-//
-// Created by minhtk on 15/03/2026.
-//
-
 #ifndef ONNX_CONFIGLOADER_HPP
 #define ONNX_CONFIGLOADER_HPP
+
 #include <cstddef>
 #include <string>
 #include <vector>
 
 struct OnnxSessionConfig {
-    int intra_op_num_threads;
-    int inter_op_num_threads;
-    std::string execution_mode;
-    std::string graph_optimization_level;
-    std::string log_severity_level;
+    int intra_op_num_threads = 4;
+    int inter_op_num_threads = 2;
+    std::string execution_mode        = "sequential";
+    std::string graph_optimization_level = "enable_extended";
+    std::string log_severity_level    = "warning";
 
-    struct execution_provider {
-        std::string type;
-        int device;
+    struct ExecutionProvider {
+        std::string type   = "cpu";
+        int         device = 0;
     } provider;
 
     struct MemoryConfig {
-        bool memory_pattern;
-        bool cpu_mem_arena;
-        size_t initial_chunk_size_bytes;
+        bool   memory_pattern           = true;
+        bool   cpu_mem_arena            = true;
+        size_t initial_chunk_size_bytes = 1048576;
+        size_t max_mem                  = 0;
+        int    arena_extend_strategy    = 0;
     } memory;
 };
 
 struct TensorInfo {
     std::vector<std::string> shape;
-    std::string type;
+    std::string              type;
 };
 
 struct TextEmbedInputs {
@@ -38,14 +37,24 @@ struct TextEmbedInputs {
 };
 
 struct TextEmbedConfig {
-    std::string model_path;
-    TextEmbedInputs inputs;
-    TensorInfo outputs;
+    std::string       model_path;
+    std::string       vocab_path = "model/tokenizer/vocab.txt";
+    TextEmbedInputs   inputs;
+    TensorInfo        outputs;
     OnnxSessionConfig session_config;
 };
 
-struct ImageEmbedConfig {
-    std::string PathFile;
+struct VisionEmbedInputs {
+    TensorInfo pixel_values;
 };
 
-#endif //ONNX_CONFIGLOADER_HPP
+struct VisionEmbedConfig {
+    std::string        model_path;
+    VisionEmbedInputs  inputs;
+    TensorInfo         outputs;
+    OnnxSessionConfig  session_config;
+    int image_width  = 224;
+    int image_height = 224;
+};
+
+#endif
